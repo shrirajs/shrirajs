dd918056801cc02f5592ae98b8def7ddf1de33bf
shriraj77 <shrirajpatil8685@gmail.com>
2022-02-24 14:42:23 +0530

    first commit

---
+++ src/main/java/com/cdac/service/CategoryService.java
@@ -0,0 +1,14 @@
+package com.cdac.service;
+
+import java.util.List;
+
+import com.cdac.model.Category;
+
+public interface CategoryService {
+	public void createCategory(Category cat);//to sace tourpackage
+	public Category findById(int id);//to find out tour package based on primary key id
+	public List<Category> getCategory();//to get all tour packages
+	public Category update(int id,String catname);//update the tour package based od id
+	public void deletetCategoryById(int id);//to delete tour pack
+
+}
